8b1192ff133eedb696448edb8a1dd019f7999c07
Éric <ceathramh@gmail.com>
2017-06-29 23:43:46 +0200

    Move atoll map to its own component

--- src/scripts/hub.js
+++ src/scripts/hub.js
@@ -2,8 +2,8 @@ import React from "react";
 import Crossroads from "./../components/Crossroads.js";
 import Funnel from "./../components/Funnel.js";
 import {useItem, acquireItem, endGame, repeatingFunnel, coatSentence} from "./helpers.js";
-import atollMap from "./../images/map.jpg";
-import { Image } from "react-bootstrap";
+import atollMapImg from "./../images/map.jpg";
+import AtollMap from "./../components/AtollMap.js";
 
 const timeLimit = 12;
 
@@ -205,38 +205,7 @@ const getIslandMap = (goToSection, flags, updateFlag) => {
   });
 
   return (
-    <div>
-      <Image src={atollMap} className="center-block visible-xs visible-xm" responsive />
-      <svg
-        id="atoll-map"
-        width="720" height="720"
-        className="center-block hidden-xs hidden-xm"
-        xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-      >
-        <image xlinkHref={atollMap} x="0" y="0" height="720" width="720"/>
-        {islands.map((island) => {
-          let className = "island";
-          if (island.current) {
-            className += " current";
-          }
-          if (island.disabled) {
-            className += " disabled";
-          }
-
-          return (
-            <g key={island.key} className={className}>
-              <path d={island.path} className="shape" onClick={island.onClick} />
-              <text x={island.textPosition.x} y={island.textPosition.y} className={`lead text-anchor-${island.textAnchor}`}>{island.description}</text>
-              {island.current && <text x={island.harbor.x} y={island.harbor.y} className="here">{`⚓`}</text>}
-              {island.disabled && island.cross && <g className="crossed">
-                <line x1={island.cross[0]} y1={island.cross[1]} x2={island.cross[2]} y2={island.cross[3]} />
-                <line x1={island.cross[4]} y1={island.cross[5]} x2={island.cross[6]} y2={island.cross[7]} />
-              </g>}
-            </g>
-          );
-        })}
-      </svg>
-    </div>
+    <AtollMap mapImg={atollMapImg} islands={islands} />
   );
 }
 
