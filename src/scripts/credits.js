15423710c0f88725a6e0e4557579e0ded4842695
Éric <ceathramh@gmail.com>
2017-10-28 11:21:44 +0200

    Credit GUI artist

--- src/scripts/credits.js
+++ src/scripts/credits.js
@@ -4,9 +4,11 @@ const credits = `
 
 <p>Initialement écrite pour le <a target="_blank" rel="noopener noreferrer" href="http://litteraction.fr/presentation/livre-dont-on-est-le-heros/resultats-detailles-du-concours-yaztromo-2011">concours Yaztromo, édition 2011</a>.</p>
 
-<p>Texte par Outremer.</p>
+<p>Texte et carte par Outremer.</p>
 
 <p>Illustrations par <a target="_blank" rel="noopener noreferrer" href="http://klauspillon.deviantart.com/">Klaus Pillon</a>, originellement dans le cadre du projet <em>Gamebook Store</em>.</p>
+
+<p>Autres éléments graphiques par <a target="_blank" rel="noopener noreferrer" href="https://lemmasoft.renai.us/forums/viewtopic.php?t=40027">WinterMochie</a>.</p>
 
 <p>Code par Skarn.</p>
 </div>
